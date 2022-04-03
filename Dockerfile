4f412d3742d328d20ca6623cd12e0ac965c45144
Youngho Cho <dominoyh5@gmail.com>
2022-04-03 14:50:12 +0900

    redis count

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM python:3-alpine
 
 MAINTAINER Youngho <dominoyh5@gmail.com>
 
+ENV REDIS_HOST=redis
+
 RUN \
     pip install fastapi && \
-    pip install uvicorn
+    pip install uvicorn && \
+    pip install redis
 
 EXPOSE 8000
 
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import os
 import socket
+import redis
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return socket.gethostname()
+    try:
+        rd = redis.StrictRedis(host=os.environ["REDIS_HOST"], port=6379, db=0)
+
+        rd.hincrby("count", socket.gethostname(), 1)
+        return rd.hgetall("count")
+    except:
+        return socket.gethostname()
